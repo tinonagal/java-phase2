50e63b1885a0ba67d0ecb6bcfe842e4d3764a56f
tinonagal <tinonagal@gmail.com>
2023-04-18 12:47:35 +0000

    added servlet example

---
+++ DynamicWebProjectExample/src/com/simplilearn/servlet/ServletExample.java
@@ -0,0 +1,46 @@
+package com.simplilearn.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletExample
+ */
+@WebServlet("/ServletExample")
+public class ServletExample extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ServletExample() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter prObj = response.getWriter();
+		prObj.print("<html><body>");
+		prObj.print("<h3>SERVLET IS AWESOME. IT HELPS US IN WEB DEVELOPMENT!!!</h3>");
+		prObj.print("</html></body>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
